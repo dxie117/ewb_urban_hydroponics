f2094fcff338225f042452d7cb932bc88a6cf40f
Jonater <jnnguyen705@gmail.com>
2019-10-21 12:49:55 -0400

    unzipped ph probe files

---
+++ pH_probe/README.txt
@@ -0,0 +1,12 @@
+to use the code you must install the library.
+to install the library you can follow this pdf: https://www.atlas-scientific.com/_files/code/gravity-pH-ardunio-code.pdf
+
+if you are too lazy to open it, here is the plain text instructions:
+1. drag atlas_gravity.zip to program files (x86) -> arduino -> libraries
+2. open arduino IDE and on the top drop down menu select Sketch -> Include Library -> Add .zip Library, then navigate to atlas_gravity.zip and select it.
+
+now you can run pH_code.ino. 
+
+if you lose it, its the example code loaded with the library.
+to retrieve the example code, in the arduino IDE:
+on the top drop down menu select: File > Examples > Atlas_gravity > Examples > pH_grav_example
---
+++ pH_probe/pH_code.ino
@@ -0,0 +1,61 @@
+/* 
+ *  Basic pH reading code stolen from the pH probe's library. 
+ */
+
+
+#include "ph_grav.h"                                  //header file for Atlas Scientific gravity pH sensor
+
+String inputstring = "";                              //a string to hold incoming data from the PC
+boolean input_string_complete = false;                //a flag to indicate have we received all the data from the PC
+char inputstring_array[10];                           //a char array needed for string parsing
+Gravity_pH pH = A0;                                   // assign analog pin A0 of Arduino to class Gravity_pH. connect output of pH sensor to pin A0
+
+
+
+void setup() {
+  Serial.begin(9600);                                 // enable serial port
+  if (pH.begin()) { Serial.println("Loaded EEPROM");} 
+  Serial.println(F("Use commands \"CAL,4\", \"CAL,7\", and \"CAL,10\" to calibrate the circuit to those respective values"));
+  Serial.println(F("Use command \"CAL,CLEAR\" to clear the calibration"));
+ }
+
+
+void serialEvent() {                                  //if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC
+}
+
+
+void loop() {
+
+  if (input_string_complete == true) {                //check if data received
+    inputstring.toCharArray(inputstring_array, 30);   //convert the string to a char array
+    parse_cmd(inputstring_array);                     //send data to pars_cmd function
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+    inputstring = "";                                 //clear the string
+  }
+  Serial.println(pH.read_ph());                       //output pH reading to serial monitor
+  delay(1000);
+}
+
+
+void parse_cmd(char* string) {                      //For calling calibration functions
+  strupr(string);                                   //convert input string to uppercase
+
+  if (strcmp(string, "CAL,4") == 0) {               //compare user input string with CAL,4 and if they match, proceed
+    pH.cal_low();                                   //call function for low point calibration
+    Serial.println("LOW CALIBRATED");
+  }
+  else if (strcmp(string, "CAL,7") == 0) {          //compare user input string with CAL,7 and if they match, proceed
+    pH.cal_mid();                                   //call function for midpoint calibration
+    Serial.println("MID CALIBRATED");
+  }
+  else if (strcmp(string, "CAL,10") == 0) {         //compare user input string with CAL,10 and if they match, proceed
+    pH.cal_high();                                  //call function for highpoint calibration
+    Serial.println("HIGH CALIBRATED");
+  }
+  else if (strcmp(string, "CAL,CLEAR") == 0) {      //compare user input string with CAL,CLEAR and if they match, proceed
+    pH.cal_clear();                                 //call function for clearing calibration
+    Serial.println("CALIBRATION CLEARED");
+  }
+}
