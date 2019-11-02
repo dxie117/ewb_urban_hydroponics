c9764053feb4abd7a3d0098b117723884144ae5a
William Zhao <williamzhao@MacBook-Pro-5.fios-router.home>
2019-11-02 16:05:21 -0400

    Moved arduino code to sketch folder

--- temp_sensor/arduino_UNO_RTD_sample_code/arduino_UNO_RTD_sample_code.ino
+++ temp_sensor/arduino_UNO_RTD_sample_code/arduino_UNO_RTD_sample_code.ino
@@ -9,8 +9,23 @@
 
 
 #include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
-#define rx 2                                          //define what pin rx is going to be
-#define tx 3                                          //define what pin tx is going to be
+
+//Testing port definitions
+//Arduino RX: 2
+//Arduino TX: 3
+
+//Rx port of circuit connects to tx port of Arduino
+//Wire circuit rx port to Arduino Digital Pin 3
+
+//Tx port of circuit connects to rx port of Arduino
+//Wire circuit tx port to Arduino Digital Pin 2
+
+//define what pin rx is going to be
+#define rx 2
+
+//define what pin tx is going to be
+#define tx 3
+
 
 SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
 
